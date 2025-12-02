7bf20cae475c22de1ddea56dd199a24776e76d5a
pahinets <tovstisasha@gmail.com>
2025-12-02 11:32:31 +0200

    Refactor update method to use switch statement

--- MKR/UIModule.java
+++ MKR/UIModule.java
@@ -3,12 +3,15 @@ package MKR;
 public class UIModule implements ISubscriber {
     @Override
     public void update(String message) {
-        if (message.equals("user_login")) {
-            System.out.println("[UI] UserInterface: Ласкаво просимо!");
-        } else if (message.equals("access_error")) {
-            System.out.println("[UI] UserInterface: Увага! Помилка доступу.");
-        } else {
-            System.out.println("[UI] Отримано системне сповіщення: " + message);
+        switch (message) {
+            case "user_login":
+                System.out.println("[UI] >> UserInterface: Ласкаво просимо в систему!");
+                break;
+            case "access_error":
+                System.out.println("[UI] >> UserInterface: УВАГА! Доступ заборонено.");
+                break;
+            default:
+                System.out.println("[UI] >> Отримано повідомлення: " + message);
         }
     }
 }
