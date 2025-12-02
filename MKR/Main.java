889de4b56fee3eb390e19667aa1a7b9fa8229c55
pahinets <tovstisasha@gmail.com>
2025-12-02 11:33:06 +0200

    Update comments for clarity in Main.java

--- MKR/Main.java
+++ MKR/Main.java
@@ -2,22 +2,21 @@ package MKR;
 
 public class Main {
     public static void main(String[] args) {
-        // 1. Створення компонентів
+        // 1. Ініціалізація компонентів
         LoggerModule logger = new LoggerModule();
         UIModule ui = new UIModule();
         AuthModule auth = new AuthModule();
 
-        // 2. Підписка компонентів через Singleton
+        // 2. Отримання єдиного екземпляра менеджера (Singleton)
         EventManager manager = EventManager.getInstance();
+
+        // 3. Підписка компонентів (Observer logic)
         manager.subscribe(logger);
         manager.subscribe(ui);
         manager.subscribe(auth);
 
-        // 3. Демонстрація роботи
-        // Сценарій: Успішний вхід
-        auth.loginUser("Oleg");
-
-        // Сценарій: Помилка
+        // 4. Демонстрація роботи системи
+        auth.loginUser("Student_MKR");
         auth.triggerError();
     }
 }
