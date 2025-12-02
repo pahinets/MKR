88aab2767678ba73b5709422e0c4b13526bae742
pahinets <tovstisasha@gmail.com>
2025-12-02 11:31:26 +0200

    Add Observer pattern documentation to ISubscriber
    Added documentation for the ISubscriber interface.

--- MKR/ISubscriber.java
+++ MKR/ISubscriber.java
@@ -1,5 +1,9 @@
 package MKR;
 
+/**
+ * ПАТЕРН: Observer (Спостерігач)
+ * Інтерфейс для всіх компонентів, які хочуть отримувати сповіщення.
+ */
 public interface ISubscriber {
     void update(String message);
 }
